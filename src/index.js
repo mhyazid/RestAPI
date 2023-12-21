bb4559a70cbc3fb1e456b534aabd0f36296ebd06
mhyazid <hadyan19yazid@gmail.com>
2023-12-21 20:43:23 +0700

    commit change

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require('express')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 const usersRoutes = require('./routes/users')
 const signInRoutes = require('./routes/signin')
 const loginRoute = require('./routes/login')
@@ -11,6 +11,9 @@ const app = express()
 
 
 app.use(express.json())
+app.get('/',(req,res)=>{
+    res.json({message:"API Bekerja"})
+})
 app.use('/users',usersRoutes);
 app.use('/signin',signInRoutes);
 app.use('/login',loginRoute);
